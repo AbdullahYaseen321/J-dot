db90784eaa1cb4ab83bb205bfe27212ed4b2f648
Abdullah <mianabdullahyaseen2001@gmail.com>
2026-04-10 13:25:26 +0500

    trying

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/J-dot/", // 👈 VERY IMPORTANT for GitHub Pages
+  base: "/J-dot/", // 👈 must
 })
